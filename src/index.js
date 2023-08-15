7c114658c0d0fc75d2828f8e495f808757b86139
mr07s <106861707+mr07s@users.noreply.github.com>
2023-08-15 12:17:38 +0530

    bug fixed

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import reducers from "./reducers";
-import { configureStore } from "@reduxjs/toolkit";
-import { applyMiddleware, compose } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
 import { store } from "./store/configureStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
+    {/* <Provider> */}
     <React.StrictMode>
       <BrowserRouter>
         <App />
