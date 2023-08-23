08f1162c9981c2ff77431df6c6cf7404d4b90bfd
Sayantan102002 <62782051+Sayantan102002@users.noreply.github.com>
2023-08-24 02:07:20 +0530

    Invoice rough design completed

--- src/Components/Invoice/MyDocument.js
+++ src/Components/Invoice/MyDocument.js
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
     alignItems: "stretch",
 
     // width: "100%",
-    border: "1px solid red",
+    // border: "1px solid red",
   },
   buyerDetails_col_1: {
     display: "flex",
@@ -103,7 +103,8 @@ const styles = StyleSheet.create({
   },
   priceDetails: {
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
+    border: "1px solid red",
   },
   price_heading_view: {
     display: "flex",
@@ -115,14 +116,40 @@ const styles = StyleSheet.create({
     borderRight: "1px solid black",
     borderBottom: "1px solid black",
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "centre",
-    padding: 5,
+    justifyContent: "center",
+    textAlign: "center",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
   },
   price_details_text: {
     fontSize: 10,
     fontWeight: "ultrabold",
     paddingHorizontal: 2,
+  },
+  itemsRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  individual_items_view: {
+    borderRight: "1px solid black",
+    borderBottom: "1px solid black",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "centre",
+    justifyContent: "center",
+    textAlign: "center",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  items_text: {
+    fontSize: 10,
+    fontWeight: "ultrabold",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    textOverflow: "clipped",
   },
 });
 
@@ -145,7 +172,7 @@ export const MyDocument = () => (
           </View>
         </View>
         <View style={styles.documentType}>
-          <Text style={{ fontWeight: "bold" }}>Invoice</Text>
+          <Text style={{ fontWeight: "bold" }}>Delivery Challan</Text>
         </View>
         {/* CONSIGNEE DETAILS */}
         <View style={styles.buyerDetails}>
@@ -198,7 +225,7 @@ export const MyDocument = () => (
             <Text style={styles.text}></Text> */}
           </View>
         </View>
-        <View style={styles.priceDetails}>
+        {/* <View style={styles.priceDetails}>
           <View style={styles.price_heading_view}>
             <View style={styles.individual_header_view}>
               <Text style={styles.price_details_text}>SL No.</Text>
@@ -225,15 +252,359 @@ export const MyDocument = () => (
             </View>
             <View style={styles.individual_header_view}>
               <Text style={styles.price_details_text}>IGST</Text>
-              <View>
-                <Text style={styles.price_details_text}>Rate</Text>
-              </View>
-              <View>
-                <Text style={styles.price_details_text}>Amt</Text>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                <View>
+                  <Text style={styles.price_details_text}>Rate</Text>
+                </View>
+                <View>
+                  <Text style={styles.price_details_text}>Amt</Text>
+                </View>
               </View>
             </View>
             <View style={styles.individual_header_view}>
               <Text style={styles.price_details_text}>Total</Text>
+            </View>
+          </View>
+          <View style={styles.itemsRow}>
+            <View style={styles.individual_items_view}>
+              <Text style={styles.price_details_text}>1</Text>
+            </View>
+            <View style={styles.individual_items_view}>
+              <Text style={styles.price_details_text}>ULSOL-O GEL SYRUP</Text>
+            </View>
+            <Text style={styles.price_details_text}>3004</Text>
+            <View style={styles.individual_items_view}>
+              <Text style={styles.price_details_text}>100</Text>
+            </View>
+            <Text style={styles.price_details_text}>1x200 ml</Text>
+            <View style={styles.individual_items_view}>
+              <Text style={styles.price_details_text}>22</Text>
+            </View>
+            <Text style={styles.price_details_text}>2200.00</Text>
+            <View style={styles.individual_items_view}>
+              <Text style={styles.price_details_text}>12.00% </Text>
+            </View>
+            <Text style={styles.price_details_text}>264.00</Text>
+            <View style={styles.individual_items_view}>
+              <Text style={styles.price_details_text}>2,464.00</Text>
+            </View>
+          </View>
+        </View> */}
+        <View
+          style={{
+            width: "100%",
+            border: "1px soild black",
+          }}
+        ></View>
+
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text style={styles.items_text}>SR No.</Text>
+            <Text style={styles.items_text}>1</Text>
+            <Text style={styles.items_text}>2</Text>
+            <Text style={styles.items_text}>3</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: 150,
+            }}
+          >
+            <Text style={styles.items_text}>Description Of Product </Text>
+            <Text style={styles.items_text}>
+              ULSOL-O GEL SYRUPdddddddddddddddddddddddddd{" "}
+            </Text>
+            <Text style={styles.items_text}>ULSOL-O GEL SYRUP </Text>
+            <Text style={styles.items_text}>ULSOL-O GEL SYRUP </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text style={styles.items_text}>HSN Code</Text>
+            <Text style={styles.items_text}>3004</Text>
+            <Text style={styles.items_text}>3004</Text>
+            <Text style={styles.items_text}>3004</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text style={styles.items_text}>QTY</Text>
+            <Text style={styles.items_text}>100</Text>
+            <Text style={styles.items_text}>100</Text>
+            <Text style={styles.items_text}>100</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text style={styles.items_text}>Unit</Text>
+            <Text style={styles.items_text}>1x200 ml</Text>
+            <Text style={styles.items_text}>1x200 ml</Text>
+            <Text style={styles.items_text}>1x200 ml</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text style={styles.items_text}>Rate</Text>
+            <Text style={styles.items_text}>22</Text>
+            <Text style={styles.items_text}>22</Text>
+            <Text style={styles.items_text}>22</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text style={styles.items_text}>Taxable Amt</Text>
+            <Text style={styles.items_text}>2200.00</Text>
+            <Text style={styles.items_text}>2200.00</Text>
+            <Text style={styles.items_text}>2200.00</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text style={styles.items_text}>IGST</Text>
+            <Text style={styles.items_text}>12.00%</Text>
+            <Text style={styles.items_text}>12.00%</Text>
+            <Text style={styles.items_text}>12.00%</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text style={styles.items_text}>Total</Text>
+            <Text style={styles.items_text}>2464.00</Text>
+            <Text style={styles.items_text}>2464.00</Text>
+            <Text style={styles.items_text}>2464.00</Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            width: "100%",
+            border: "1px soild black",
+          }}
+        ></View>
+
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingLeft: 100,
+            }}
+          >
+            <Text style={styles.items_text}>Total</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingLeft: 120,
+            }}
+          >
+            <Text style={styles.items_text}>300</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingLeft: 98,
+            }}
+          >
+            <Text style={styles.items_text}>6600.00</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingLeft: 25,
+            }}
+          >
+            <Text style={styles.items_text}>792</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingLeft: 5,
+            }}
+          >
+            <Text style={styles.items_text}>7392.00</Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            width: "100%",
+            border: "1px soild black",
+          }}
+        ></View>
+
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            border: "1px solid black",
+            width: "100%",
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid black",
+              width: "60%",
+            }}
+          >
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                borderBottom: "1px solid black",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                minHeight: 100,
+              }}
+            >
+              <Text style={styles.items_text}>Total Amount in words</Text>
+              <Text style={styles.items_text}>
+                Seven Thousand Three Hundred and Ninety Two Only{" "}
+              </Text>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                // border: "1px solid black",
+                width: "100%",
+              }}
+            >
+              <Text style={styles.items_text}>Terms and Conditions</Text>
+              <Text style={styles.items_text}>
+                1. This is an electronically generated document
+              </Text>
+              <Text style={styles.items_text}>
+                2. All disputes are subject to BERHAMPORE jurisdiction
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "40%",
+            }}
+          >
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                // border: "1px solid black",
+                width: "100%",
+              }}
+            >
+              <Text style={styles.items_text}>Total Taxable Amount :</Text>
+              <Text style={styles.items_text}>Rs 6600.00</Text>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                // border: "1px solid black",
+                width: "100%",
+              }}
+            >
+              <Text style={styles.items_text}>Total GST :</Text>
+
+              <Text style={styles.items_text}>Rs 792.00</Text>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                // border: "1px solid black",
+                width: "100%",
+              }}
+            >
+              <Text style={styles.items_text}>Total Amount After Tax :</Text>
+
+              <Text style={styles.items_text}>Rs 7392.00</Text>
+            </View>
+
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                alignContent: "center",
+                border: "1px solid black",
+                width: "100%",
+                minHeight: 150,
+              }}
+            >
+              <View>
+                <Text style={styles.items_text}>
+                  Certified that the particular given above are true and correct
+                </Text>
+                <Text style={styles.items_text}>For, SHEERINA ENTERPRISE</Text>
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  // alignContent: "center",
+                  // border: "1px solid black",
+                  width: "100%",
+                  // minHeight: 150,
+                }}
+              >
+                <Text style={styles.items_text}>Authorized Signatory</Text>
+              </View>
+              s
             </View>
           </View>
         </View>
